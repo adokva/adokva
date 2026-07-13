cbaaf6217269dee33dbfbd683fb17591f964a874
adokva <yadokvay@gmail.com>
2026-07-14 01:13:50 +0400

    Create NightLights.tsx

---
+++ components/components/NightLights.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useTexture } from "@react-three/fiber";
+
+export default function NightLights() {
+  const lightsTexture = useTexture(
+    "/textures/night.jpg"
+  );
+
+  return (
+    <mesh>
+      <sphereGeometry
+        args={[1.505, 64, 64]}
+      />
+
+      <meshBasicMaterial
+        map={lightsTexture}
+        transparent
+        opacity={0.7}
+      />
+    </mesh>
+  );
+}

dfc9ecd66907f284e457c8efa6dff78138f30da3
adokva <yadokvay@gmail.com>
2026-07-14 00:42:21 +0400

    Add UI component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Scene from "../components/Scene";
+import UI from "../components/UI";
 
 export default function Home() {
   return (
     <main>
       <Scene />
+      <UI />
     </main>
   );
 }

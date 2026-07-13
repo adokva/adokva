4dae0c98b2ef58d4a0045812bc0e31f861af2354
adokva <yadokvay@gmail.com>
2026-07-14 00:34:44 +0400

    Create Scene.tsx

---
+++ components/Scene.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Globe from "./Globe";
+
+export default function Scene() {
+  return (
+    <div>
+      <Globe />
+
+      <p>
+        Space Engine Loading...
+      </p>
+    </div>
+  );
+}

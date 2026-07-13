d0febbcf370265254a9fd9658d26d21b24331d86
adokva <yadokvay@gmail.com>
2026-07-14 00:41:37 +0400

    Create UI.tsx

---
+++ components/UI.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function UI() {
+  return (
+    <div>
+      <h1>
+        Adokva 🌍
+      </h1>
+
+      <p>
+        Explore the planet
+      </p>
+    </div>
+  );
+}

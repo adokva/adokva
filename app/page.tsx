63964d1656c1e57c207ab662c50b55fddbe27b0d
adokva <yadokvay@gmail.com>
2026-07-14 00:27:15 +0400

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        Adokva 🌍
+      </h1>
+
+      <p>
+        3D planet exploration begins.
+      </p>
+    </main>
+  );
+}

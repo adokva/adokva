89f23955b73cd66fe62374fafa773c06b4f58aec
adokva <yadokvay@gmail.com>
2026-07-14 01:28:31 +0400

    Update NightLights.tsx

--- components/components/NightLights.tsx
+++ components/components/NightLights.tsx
@@ -3,20 +3,16 @@
 import { useTexture } from "@react-three/fiber";
 
 export default function NightLights() {
-  const lightsTexture = useTexture(
-    "/textures/night.jpg"
-  );
+  const lightsTexture = useTexture("/textures/night.jpg");
 
   return (
     <mesh>
-      <sphereGeometry
-        args={[1.505, 64, 64]}
-      />
+      <sphereGeometry args={[1.505, 64, 64]} />
 
       <meshBasicMaterial
         map={lightsTexture}
         transparent
-        opacity={0.7}
+        opacity={0.45}
       />
     </mesh>
   );

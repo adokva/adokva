0727c73b1f1bb0235b7172141a709ecf94203519
adokva <yadokvay@gmail.com>
2026-07-14 00:50:35 +0400

    Refactor Atmosphere component to use Three.js mesh

--- components/Atmosphere.tsx
+++ components/Atmosphere.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import * as THREE from "three";
+
 export default function Atmosphere() {
   return (
-    <div>
-      💙
-      <p>
-        Earth atmosphere module
-      </p>
-    </div>
+    <mesh>
+      <sphereGeometry
+        args={[1.58, 64, 64]}
+      />
+
+      <meshBasicMaterial
+        color="#4da6ff"
+        transparent
+        opacity={0.18}
+        side={THREE.BackSide}
+      />
+    </mesh>
   );
 }

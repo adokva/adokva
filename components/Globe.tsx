5d17a73dfd891bb76fc5149a14b583df8412de6f
adokva <yadokvay@gmail.com>
2026-07-14 00:49:34 +0400

    Implement globe rotation in Globe component
    Added rotation to the globe mesh using useFrame hook.

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -2,13 +2,22 @@
 
 import { useRef } from "react";
 import { Mesh } from "three";
+import { useFrame } from "@react-three/fiber";
 
 export default function Globe() {
   const globe = useRef<Mesh>(null);
 
+  useFrame(() => {
+    if (globe.current) {
+      globe.current.rotation.y += 0.002;
+    }
+  });
+
   return (
     <mesh ref={globe}>
-      <sphereGeometry args={[1.5, 64, 64]} />
+      <sphereGeometry
+        args={[1.5, 64, 64]}
+      />
 
       <meshStandardMaterial
         color="#1d8cff"
